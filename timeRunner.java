3938375586a29ac36a5ba15bdc0d1493c7a23182
DJCherfils <146093722+DJCherfils@users.noreply.github.com>
2024-01-21 14:50:42 -0500

    Update timeRunner.java

--- timeRunner.java
+++ timeRunner.java
@@ -4,8 +4,7 @@ public class timeRunner{
         System.out.println(time1.info());
         time1.tick();
         System.out.println(time1.info());
-        time1.tick();
-        System.out.println(time1.info());
+          System.out.println(time1.info());
         time1.tick();
         System.out.println(time1.info());
         time1.tick();
@@ -25,10 +24,22 @@ public class timeRunner{
         System.out.println(time2.info());
         time2.tick();
         System.out.println(time2.info());
-
-
-
-
-
+        time2.tick();
+        System.out.println(time2.info());
+        Time time3 = new Time(23, 59, 58);
+        System.out.println(time3.info());
+        time3.tick();
+        System.out.println(time3.info());
+        time3.tick();
+        System.out.println(time3.info());
+        Time time4 = new Time(10, 14, 43);
+        System.out.println(time4.info());
+        Time time5 = new Time(8, 30, 29);
+        time4.add(time5);
+        System.out.println(time4.info());
+        System.out.println(time5.info());
+        Time time6 = new Time(7, 20, 0);
+        time4.add(time6);
+        System.out.println(time4.info());
     }
 }
